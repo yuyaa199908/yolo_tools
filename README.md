12ef6cb2ac3b9c4ce0b0bd7c317fa0ab82dc4df7
yuyaa199908 <yuyaa199908@gmail.com>
2024-06-25 12:51:09 +0900

    add launch and config

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # 
+ros2 launch realsense2_camera rs_launch.py enable_rgbd:=true enable_sync:=true align_depth.enable:=true enable_color:=true enable_depth:=true`
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ package_name = 'yolo_tools'
 setup(
     name=package_name,
     version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    packages=[package_name],    #find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
